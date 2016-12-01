67c3cbe2a0f89259e3f6c825532d04e808439572
Laurent Prevost <laurent.prevost@probedock.io>
2016-12-01 11:24:58 +0100

    Try to fix auto restart of the containers

---
+++ images/probedock-job/config/cont-init.d/wait.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+export PGPASSWORD="$PROBEDOCK_DATABASE_PASSWORD"
+until psql -h $PROBEDOCK_DATABASE_HOST -U "$PROBEDOCK_DATABASE_USERNAME" -c '\l'; do
+  >&2 echo "Postgres is unavailable - waiting"
+  sleep 1
+done
+
+>&2 echo "Postgres is up"
+
+REDIS_ADDRESS=$(echo "$PROBEDOCK_REDIS_URL"|cut -d : -f 1)
+REDIS_PORT=$(echo "$PROBEDOCK_REDIS_URL"|cut -d : -f 2)
+until (echo > "/dev/tcp/$REDIS_ADDRESS/$REDIS_PORT") >/dev/null 2>&1; do
+  >&2 echo "Redis is unavailable - waiting"
+  sleep 1
+done
+
+>&2 echo "Redis is up"
+
+exec "$@"
